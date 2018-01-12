14e4c1fec9f1172b65df03cc514a463032ab6889
Nolan Byrnes <nolanehs@gmail.com>
2018-01-12 23:41:58 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -20,6 +20,14 @@ class Application
       @@cart.each do |item|
         resp.write "#{item}\n"
       end
+elsif req.path.match(/add/)
+  search_term = req.params["item"]
+if @@items.include?(search_term)
+  @@cart << search_term
+  resp.write "added #{search_term}"
+else
+  resp.write "We don't have that item"
+end
 
     else
       resp.write "Path Not Found"
